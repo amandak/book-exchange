14d5a5567894f3fc59eeb634d87565b8a7b91d00
PinalPatel <pinpat90@uga.edu>
2014-04-08 17:34:22 -0400

    edited and tested UserHelper class and all methods are working

--- book-exchange/test_src/db/UserHelperTest.java
+++ book-exchange/test_src/db/UserHelperTest.java
@@ -16,7 +16,7 @@ public class UserHelperTest extends TestCase {
        	* Verify that user listing is successfully added to database and data is accurate
        	*/
        	//Create new user object
-		int userId = 1;
+		
 		String username = "ronmcdon";
 		String role = "user";
 		String firstName = "Ronald";
@@ -28,13 +28,18 @@ public class UserHelperTest extends TestCase {
 		int zip = 30601;
 		String country = "USA";
 		
-		User newUser = new User(userId, username, role, firstName, lastName, emailAddress, streetAddress, city, state, zip, country);
+		User newUser = new User(username, role, firstName, lastName, emailAddress, streetAddress, city, state, zip, country);
        	
        	/**
        	 * Open connection to database by creating object of UserHelper
        	 * and add new user to the table
        	 */
-       	UserHelper helper = new UserHelper();
+       	UserHelper helper = null;
+		try {
+			helper = new UserHelper();
+		} catch (Exception e) {
+			System.out.println(e.getClass().getName() + " Initializing UserHelper object: " + e.getMessage());
+		}
        	
        	
 		//add user
@@ -46,23 +51,22 @@ public class UserHelperTest extends TestCase {
        	User userFromDatabase = helper.getUser("ronmcdon");
     
        	// Assert that the data returned is valid
-       	assertEquals("User ID", 1, userFromDatabase.getUserId());
        	assertEquals("User Name", "ronmcdon", userFromDatabase.getUserName());
        	assertEquals("Role", "user", userFromDatabase.getRole());
        	assertEquals("FirstName", "Ronald", userFromDatabase.getFirstName());
        	assertEquals("Last Name", "McDonald", userFromDatabase.getLastName());
-       	assertEquals("Email Address", "mc@donald.com", userFromDatabase.getEmailAddress());
+       	assertEquals("Email Address", "mc@donalds.com", userFromDatabase.getEmailAddress());
        	assertEquals("Street Address", "123 Main Street", userFromDatabase.getStreetAddress());
        	assertEquals("City", "Athens", userFromDatabase.getCity());
        	assertEquals("State", "GA", userFromDatabase.getState());
-       	assertEquals("Zip", "30601", userFromDatabase.getZip());
+       	assertEquals("Zip", 30601, userFromDatabase.getZip());
        	assertEquals("Country", "USA", userFromDatabase.getCountry());
        	
        	
        	/*
        	 * Verify that verifyLogin returns the correct boolean based on whether credentials are valid.
        	 */
-       	boolean verifyWorks = helper.verifyLogin("username", "password");
+       	boolean verifyWorks = helper.verifyLogin("ronmcdon", "password");
        	assertTrue("Login succeeded", verifyWorks);
        	
        	
@@ -77,7 +81,9 @@ public class UserHelperTest extends TestCase {
        	 */
        	String newPassword = "newpassword";		
 		helper.updateUserPassword("ronmcdon", newPassword);
-		boolean verified = helper.verifyLogin("romcdon", "newpassword");
+		boolean verified = helper.verifyLogin("ronmcdon", "newpassword");
 		assertTrue("Password changed successfully", verified);
+		
+		helper.closeConnection();
 	}
 }
