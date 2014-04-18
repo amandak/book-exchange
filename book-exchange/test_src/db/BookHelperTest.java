d90dbc10e23cbde7876903b1f3126906ab6b56b8
PinalPatel <pinpat90@uga.edu>
2014-04-18 16:51:39 -0400

    added authors to BookHelperTest

--- book-exchange/test_src/db/BookHelperTest.java
+++ book-exchange/test_src/db/BookHelperTest.java
@@ -10,6 +10,7 @@ import junit.framework.TestCase;
 
 /**
  * Test class verifies that BookHelper methods accurately modify Book table in database
+ @author Vince Caparell
  */
 
 public class BookHelperTest extends TestCase {
@@ -39,12 +40,12 @@ public class BookHelperTest extends TestCase {
 		String desc = "Book in good condition. Cover shows minor signs of wear.";
 		String author = "David A. Patterson";
 		String ed = "fourth";
-		String status = "listed"; //Does this specify that book is for sale, sold, or traded?
+		String status = "listed";
 		String condition = "GOOD";
 		String department = "CSCI";
 		String className = "CSCI4720";
 
-		//Should we pass in ID as param? Or should that be automatically set by database?
+		
 		Book newBook1 = new Book(userId, bookId, title, isbn, desc, author, ed,
 				status, condition, price, className, department);
 
@@ -56,11 +57,11 @@ public class BookHelperTest extends TestCase {
 		try {
 			bookDAO = new BookHelper();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+		
 			e.printStackTrace();
 		}
 		//Method returns id
-		//Change this if we want to set id instead of it automatically getting set
+		
 		boolean added = bookDAO.addBook(newBook1); 
 		
 		assertTrue("added a book", added);
@@ -97,34 +98,7 @@ public class BookHelperTest extends TestCase {
 		assertEquals("Condition", "GOOD", bookTest.getCondition());
 		assertEquals("Price", 35.0, bookTest.getPrice());
 
-		/**
-		 * Update a current book listing and verify that modifications made to Book table are accurate
-		 */
-		/**
-		//update title bookDAO.setTitle(newID1)
-		bookDAO.setBookName(newID1, "Organizacion de la Computadora");
-		bookDAO.setISBN(newID1, "9780123747503");
-		bookDAO.setDesc(newID1, "Cover torn off. Book is in terrible shape.");
-		bookDAO.setAuthor(newID1, "John L. Hennessy");
-		bookDAO.setEdition(newID1, "third");
-		bookDAO.setStatus(newID1, ""); //Change this
-		bookDAO.setCondition(newID1, "acceptable");
-		bookDAO.setPrice(newID1, 4);
-		//Update bookTest object with the modifications
-		bookTest = bookDAO.getBookById(newID1);
-
-		assertEquals("title modified", "Organizacion de la Computadora", bookTest.getBookName());
-		assertEquals("ISBN modified", "9780123747503", bookTest.getIsbn());
-		assertEquals("Description modified", "Cover torn off. Book is in terrible shape.", bookTest.getDescription());
-		assertEquals("Author modified", "John L. Hennessy", bookTest.getAuthor());
-		assertEquals("Edition modified", "third", bookTest.getEdition());
-		assertEquals("Status modified", "", bookTest.getStatus());
-		assertEquals("Condition modified", "acceptable", bookTest.getCondition());
-		assertEquals("Price modified", 4, bookTest.getPrice());
-**/
-		//Should we have more getters in BookHelper as well? I'm not sure if we would need them.
-
-
+		
 		/**
 		 * Remove book listing from table and verify null
 		 */
